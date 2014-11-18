1f85f72fd7d69dddb4bf9adc5085ed46024f4c81
maxdavila <adavila0711@gmail.com>
2014-11-18 01:24:42 -0800

    POC, with limited stock picks

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var config = require('config-heroku');
 
 var oa = new oauth.OAuth(null, null, config.tradeking.consumer_key, config.tradeking.consumer_secret, "1.0", null, "HMAC-SHA1");
 
-var request = oa.get("https://stream.tradeking.com/v1/market/quotes.json?symbols=AAPL,MMM,ACN,TSLA",
+var request = oa.get("https://stream.tradeking.com/v1/market/quotes.json?symbols=AAPL,MMM,ACN,ALLE,APH,AVY,BA,CAT,CHRW,CTAS,GLW,CSX,CMI,DHR,ADBE,AKAM,ADS,ALTR,AME,ADI,AMAT",
               config.tradeking.access_token, 
               config.tradeking.access_secret);
 /* GET home page. */
@@ -20,17 +20,17 @@ router.get('/', function(req, res) {
     response.on('data', function(data) {
       try {
         var jsonBody = JSON.parse(data);
+        if (jsonBody.trade) {
+          io.sockets.emit('trade', { trade: jsonBody.trade })
+          console.log("trade", jsonBody.trade);
+        } else {
+          console.log("quote trade")
+        }
       }
       catch(err) {
         console.log(data)
       }
 
-      if (jsonBody.trade) {
-        io.sockets.emit('trade', { trade: jsonBody.trade })
-        console.log("trade", jsonBody.trade);
-      } else {
-        console.log("quote trade")
-      }
     })
   });
   request.end();
