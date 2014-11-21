c77275d04470625b18b6a0c082501abbc653aa0f
maxdavila <adavila0711@gmail.com>
2014-11-20 23:43:20 -0800

    adds sockethelper

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var oauth = require('oauth');
 var config = require('config-heroku');
+var SocketHelper = require('../lib/socket_helper');
 
 router.get('/', function(req, res) {
   var io = req.app.get('io');
@@ -23,28 +24,9 @@ router.post('/check', function(req, res) {
 router.get('/health_check', function(req, res) {
   var io = req.app.get('io');
   var tradeking = req.app.get('tradeking');
-  var clients = findClientsSocket(io);
+  var clients = SocketHelper.findClients(io);
 
   res.send({ current_connections: clients, requests: tradeking.requestCount });
 });
-
-function findClientsSocket(io, roomId, namespace) {
-    var res = []
-    , ns = io.of(namespace ||"/");    // the default namespace is "/"
-
-    if (ns) {
-      for (var id in ns.connected) {
-        if(roomId) {
-          var index = ns.connected[id].rooms.indexOf(roomId) ;
-          if(index !== -1) {
-            res.push(ns.connected[id].id);
-          }
-        } else {
-          res.push(ns.connected[id].id);
-        }
-      }
-    }
-    return res;
-}
 
 module.exports = router;
