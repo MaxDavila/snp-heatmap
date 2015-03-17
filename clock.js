5de650666dd9cb4445c5bad0e84d3f3397f1eaf4
maxdavila <adavila0711@gmail.com>
2015-03-17 01:57:48 -0700

    run job only right after market close

--- clock.js
+++ clock.js
@@ -2,7 +2,7 @@ var CronJob = require('cron').CronJob;
 var snpUtil = require('./snp-util');
 
 new CronJob({
-  cronTime: '00 51 04 * * 1-5',
+  cronTime: '00 01 16 * * 1-5',
   onTick: function() {
     snpUtil.performSnpUpdate();
   },
